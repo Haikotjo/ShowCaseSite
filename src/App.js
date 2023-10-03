7f5b5a6dcb33cb8faa4c7ff73a29cbe148ee011d
Haikotjo <haikowierda@hotmail.com>
2023-10-03 16:25:18 +0200

    API call werkt. Ziet er nu nog niet uit maar dat komt later wel

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {getPhotos} from "./api";
 import PhotoCard from "./PhotoCard/Photocard";
 
 function App() {
-    const [photos, setphotos] = useState()
+    const [photos, setphotos] = useState([])
 
     useEffect(()=>{
         const fetchData = async () => {
             try {
-                const photoData = await getPhotos('photos/radom', 10);
+                const photoData = await getPhotos('photos/random', 10);
                 setphotos(photoData)
             } catch (error) {
                 console.log('Er ging iets mis: ', error)
@@ -18,16 +18,16 @@ function App() {
     }, []);
 
   return (
-      <h1>hallo</h1>
-      // <div className="container">
-      //   <div className="row">
-      //       {photos.map((photo, index) => (
-      //           <div className="col-md-4" key={index}>
-      //               <PhotoCard photoUrl={photo.urls.small} text={photo.alt_description || 'Geen beschrijving'} />
-      //           </div>
-      //       ))}
-      //   </div>
-      // </div>
+      <div className="container">
+          <h1>hallo</h1>
+        <div className="row">
+            {photos.map((photo, index) => (
+                <div className="col-md-4" key={index}>
+                    <PhotoCard photoUrl={photo.urls.small} text={photo.alt_description || 'Geen beschrijving'} />
+                </div>
+            ))}
+        </div>
+      </div>
   );
 }
 
