284b7f8619ef517d3e8df8b9ec01d6d6713441bd
Haikotjo <haikowierda@hotmail.com>
2023-10-17 21:34:37 +0200

    Alles werkt naar behoren mixins goed toegepast. Logo of titel nog steeds doen. Buttons om andere plaatjes te laden ook voor morgen

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import TitleComponent from "./Titel/Title";
 
 function App() {
     const [photos, setPhotos] = useState([]);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const photoData = await getPhotos('photos/random', 12); // Aangepast voor het voorbeeld
+                const photoData = await getPhotos('photos/random', 16); // Aangepast voor het voorbeeld
                 setPhotos(photoData);
             } catch (error) {
                 console.log('Er ging iets mis: ', error);
@@ -19,22 +20,35 @@ function App() {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div>
             <TitleComponent />
-            <div className={styles['custom-container']}>
-                {photos.map((photo, index) => (
-                    <>
-                        <div className={styles['custom-col']} key={index}>
-                            <PhotoCard photoUrl={photo.urls.small} text={photo.alt_description || 'Geen beschrijving'} />
-                        </div>
-                        {(index + 1) % 3 === 0 && (
-                            <div className={`${styles['custom-col']} ${styles['full-width-card']}`} key={`wide-${index}`}>
-                                <PhotoCard isFullWidth={true} photoUrl={photo.urls.small} text={photo.alt_description || 'Geen beschrijving'} />
+            <div className={styles['center-container']}>
+                <div className={styles['custom-container']}>
+                    {photos.map((photo, index) => {
+                        const isFullWidth = windowWidth <= 767 ? true :
+                            windowWidth <= 991 ? (index + 1) % 3 === 0 :
+                                (index + 1) % 4 === 0;
+
+                        return (
+                            <div className={`${styles['custom-col']} ${isFullWidth ? styles['full-width-card'] : ''}`} key={index}>
+                                <PhotoCard isFullWidth={isFullWidth} photoUrl={photo.urls.small} text={photo.alt_description || 'Geen beschrijving'} />
                             </div>
-                        )}
-                    </>
-                ))}
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
--- src/PhotoCard/Photocard.js
+++ src/PhotoCard/Photocard.js
@@ -3,24 +3,33 @@ import styles from './Photocard.module.scss';
 
 const PhotoCard = ({ photoUrl, text, isFullWidth }) => {
     const [showMore, setShowMore] = useState(false);
-    const shortText = text.split(' ').slice(0, 5).join(' ') + '...';
+    const [showModal, setShowModal] = useState(false);
+    const shortText = text.split(' ').slice(0, 5).join(' ');
 
     return (
         <div className={`${styles['custom-card']} ${isFullWidth ? styles['full-width'] : ''}`}>
-            <div className={styles['custom-card-container']}>
+            <div className={styles['custom-card-container']} onClick={() => setShowModal(true)}>
                 <img src={photoUrl} className={styles['custom-card-img-top']} alt="..." />
             </div>
             <div className={styles['custom-card-body']}>
-                <p className={styles['custom-card-text']}>{isFullWidth ? text : (showMore ? text : shortText)}</p>
-                {!isFullWidth && (
-                    <button className={styles['custom-btn']} onClick={() => setShowMore(!showMore)}>
-                        {showMore ? 'Minder lezen' : 'Meer lezen'}
-                    </button>
-                )}
+                <p className={styles['custom-card-text']}>
+                    {isFullWidth ? text : (showMore ? text : shortText)}
+                    {!isFullWidth && !showMore && (
+                        <span onClick={() => setShowMore(true)} className={styles['clickable-ellipsis']}> >>></span>
+                    )}
+                </p>
             </div>
+            {showModal && (
+                <div className={styles['modal']} onClick={() => setShowModal(false)}>
+                    <div className={styles['modal-content']}>
+                        <img src={photoUrl} alt="..." />
+                        <p>{text}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
-}
+};
 
 
 export default PhotoCard;
